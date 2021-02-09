ac7e8b9922cc7a5dd50cd236022efef43efa4d44
Alexey <salogast@gmail.com>
2021-02-09 13:36:41 +0700

    feature(Lab_1_Project): создан класс Person с проверкой на максимальный возраст

---
+++ Lab_1_Project/Lab_1_Project/Person.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_1_Project
+{
+    public class Person
+    {
+        private uint _age;
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public uint Age 
+        { 
+            get => _age;
+            private set
+            {
+                const uint maximumValue = 118;
+                if (value >= maximumValue)
+                {
+                    throw new ArgumentOutOfRangeException($"{nameof(Age)} должен быть меньше {maximumValue}!");
+                }
+                _age = value;
+            }
+                
+        }
+        public Gender Gender { get; set; }
+    }
+}
