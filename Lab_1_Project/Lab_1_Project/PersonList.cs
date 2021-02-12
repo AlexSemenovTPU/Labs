36cc2d49ff892114282395ee145cc8bd35b3e0b6
Alexey <salogast@gmail.com>
2021-02-12 16:49:40 +0700

    feature(Lab_1_Project): созданы методы add и deleteAll

--- Lab_1_Project/Lab_1_Project/PersonList.cs
+++ Lab_1_Project/Lab_1_Project/PersonList.cs
@@ -8,5 +8,30 @@ namespace Lab_1_Project
 {
     class PersonList
     {
+        private Person[] _localPersonArray = new Person[1];
+
+        public Person[] Persons => _localPersonArray;
+
+        public int Count => _localPersonArray.Length;
+
+        /// <summary>
+        /// Добавление нового элемента
+        /// </summary>
+        public void add(Person newelement)
+        {
+            int elementCount = Count;
+
+            Array.Resize(ref _localPersonArray, Count + 1);
+            _localPersonArray[elementCount] = newelement;
+        }
+
+        /// <summary>
+        /// Очистка списка
+        /// </summary>
+        public void deleteAll()
+        {
+            Array.Clear(Persons, 0, Count);
+            Array.Resize(ref _localPersonArray, 0);
+        }
     }
 }
