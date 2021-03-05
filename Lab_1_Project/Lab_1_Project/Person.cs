2cdbe181a4563ea53a8b7d0e72699440ef99ab29
Alexey <salogast@gmail.com>
2021-03-05 22:35:44 +0700

    style(Lab_1_Project): доработан метод CheckPerson

--- Lab_1_Project/Lab_1_Project/Person.cs
+++ Lab_1_Project/Lab_1_Project/Person.cs
@@ -51,12 +51,12 @@ namespace Lab_1_Project
         /// <summary>
         /// Возраст персоны
         /// </summary>
-        private uint _age;
+        private int _age;
 
         /// <summary>
         /// Возраст персоны
         /// </summary>
-        public uint Age 
+        public int Age 
         { 
             get => _age;
             private set
@@ -91,7 +91,7 @@ namespace Lab_1_Project
         /// <param name="surnme"></param>
         /// <param name="age"></param>
         /// <param name="gender"></param>
-        public Person(string name, string surnme, uint age, Gender gender)
+        public Person(string name, string surnme, int age, Gender gender)
         {
             Name = name;
             Surname = surnme;
@@ -130,14 +130,14 @@ namespace Lab_1_Project
         /// Проверка возраста на соответствие требованиям
         /// </summary>
         /// <param name="age"></param>
-        public static void CorrectAge(uint age)
+        public static void CorrectAge(int age)
         {
-            const uint maximumValue = 118;
-            const uint minimumValue = 0;
+            const int maximumValue = 118;
+            const int minimumValue = 0;
             if ((age >= maximumValue) || (age <= minimumValue))
             {
-                throw new ArgumentOutOfRangeException(
-                    $"{nameof(Age)} должен быть меньше {maximumValue} или больше {minimumValue}!");
+                throw new ArgumentException(
+                    $"Возраст должен быть меньше {maximumValue} или больше {minimumValue}!");
             }
         }
 
