9a594f2e71119d66b734a9650b286db50dce2bd9
Alexey <salogast@gmail.com>
2021-05-21 14:36:40 +0700

    (Model): создан проект бизнес-логики

---
+++ Lab3/Model/Model/TransportBase.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    /// <summary>
+    /// Базоввый класс для всех
+    /// видов транспорта
+    /// </summary>
+    public abstract class TransportBase
+    {
+        /// <summary>
+        /// Рачет колличества затраченного топлива
+        /// </summary>
+        public abstract double FuelQuantity { get; }
+
+        public static double NumberCheck (double number)
+        {
+            if (number < 0)
+            {
+                throw new ArgumentOutOfRangeException("Величина должна" +
+                    "быть положительным числом!");
+            }
+            else
+            {
+                return number;
+            }
+        }
+
+    }
+}
